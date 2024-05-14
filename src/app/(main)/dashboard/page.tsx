fdc6fcf8b7c209eddbe98a6ad275c5e8c64beee2
hariom-ojha23 <hariomojha2301@gmail.com>
2024-05-14 12:48:15 +0530

    data table basic structure

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,12 +1,47 @@
 import Breadcrumbs from "@/components/common/Breadcrumbs"
 import Indicator from "@/components/dashboard/Indicator"
+import { Payment, columns } from "@/components/datatable/Columns"
+import { DataTable } from "@/components/datatable/Datatable"
 
-export default function Dashboard() {
+export default async function Dashboard() {
     const crumbs = [{ title: 'Dashboard', href: '/dashboard' }]
     const indicators = [
         { title: 'Total Employees', value: 56, },
         { title: 'Active Employees', value: 20, },
         { title: 'Inactive Employees', value: 36, },
+    ]
+
+    let data: Payment[] = [
+        {
+            id: "1",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+        {
+            id: "2",
+            amount: 200,
+            status: "success",
+            email: "om@example.com",
+        },
+        {
+            id: "1",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+        {
+            id: "2",
+            amount: 200,
+            status: "success",
+            email: "om@example.com",
+        },
+        {
+            id: "1",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
     ]
 
     return (
@@ -18,6 +53,10 @@ export default function Dashboard() {
                         <Indicator key={index} title={title} value={value}  />
                     ))
                 }
+            </div>
+
+            <div className="pt-10 w-[67.5%]">
+                <DataTable columns={columns} data={data} />
             </div>
         </div>
     )
